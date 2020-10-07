654f7c8b5744d75f83a5e583c9d3b9d42c1e0d5a
usuario <usuario@172.16.16.23>
2020-10-07 14:45:04 +0200

    Ejercicio 5

---
+++ PSP/src/ejercicio5/Ejecuta.java
@@ -0,0 +1,35 @@
+package ejercicio5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Ejecuta {
+
+	public static void main(String[] command) {
+        // TODO Auto-generated method stub
+        String line;
+        ProcessBuilder pb = new ProcessBuilder(command);
+        pb.redirectErrorStream(true);
+        try {
+            Process shell = pb.start();
+            InputStream is = shell.getInputStream();
+           // BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, "Cp850"));
+
+            System.out.println("La salida del proceso hijo " + Arrays.toString(command) + ":");
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+
+            }
+            is.close();
+
+
+        } catch (IOException e) {
+            System.out.println("Error ocurrió ejecutando el comando. Descripción: "+e.getMessage());
+        }
+    }
+
+}
