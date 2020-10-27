e93c48a5cbe854c5e5e3643c9e625f3f4b588663
VersusPC <VersusPC@VSPC-EternalstormV2>
2020-10-27 22:28:45 +0100

    Modificacion

--- Boletin2/src/ejercicio2/Hilo1.java
+++ Boletin2/src/ejercicio2/Hilo1.java
@@ -3,8 +3,8 @@ package ejercicio2;
 public class Hilo1 implements Runnable {
 
 	Thread t;
-	
-	Hilo1 () {
+
+	Hilo1() {
 		t = new Thread(this, "Nuevo Thread");
 		System.out.println("Creado hilo: " + t);
 	}
@@ -12,7 +12,15 @@ public class Hilo1 implements Runnable {
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
+
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			Thread.currentThread().interrupt();
+		}
+
 		System.out.println("Hola soy el thread numero 1");
-		
+
 	}
 }
--- Boletin2/src/ejercicio2/Hilo2.java
+++ Boletin2/src/ejercicio2/Hilo2.java
@@ -3,17 +3,23 @@ package ejercicio2;
 public class Hilo2 implements Runnable {
 
 	Thread t;
-	
-	Hilo2 () {
+
+	Hilo2() {
 		t = new Thread(this, "Nuevo Thread");
 		System.out.println("Creado hilo: " + t);
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		System.out.println("Hola,soy el thread número 2");
-		
+
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			Thread.currentThread().interrupt();
+		}
+
+		System.out.println("Hola soy el thread numero 2");
+
 	}
 }
-
